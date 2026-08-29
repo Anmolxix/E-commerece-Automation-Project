26384b91135f58c989ce58741c9ef9322b2453e6
Anmol <anmoldahal16@gmail.com>
2026-08-29 10:53:23 +0545

    added a new test case with invalid username

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -12,10 +12,13 @@ class Test_001_Login:
         self.driver = setup
         self.driver.get(self.baseURL) 
         act_title= self.driver.title
-        self.driver.close()
-        if act_title=="nopCommerce demo store. Login":
+        if act_title=="nopCommerce demo store. Login123":
+            self.driver.close()
             assert True
+
         else:
+                self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
+                self.driver.close()
                 assert False
 
     def test_login(self,setup):
@@ -28,10 +31,46 @@ class Test_001_Login:
 
         
         act_title= self.driver.title
-        self.driver.close()
+        
 
         if act_title=="Dashboard / nopCommerce administration":
+            self.driver.close()
             assert True
+            
 
         else:
-             assert False
+                self.driver.save_screenshot(".\\Screenshots\\"+"test_login.png")
+                self.driver.close()
+                assert False
+
+
+class Test_002_login:
+      baseURL = "https://admin-demo.nopcommerce.com/admin/"
+      username= "admin@yourstore.com22"
+      password= "admin"
+
+      def test_login2(self,setup):
+           self.driver= setup
+           self.driver.get(self.baseURL)
+           self.lp=Login(self.driver)
+           self.lp.setUserName(self.username)
+           self.lp.setPassword(self.password)
+           self.lp.clickLogin()
+
+           act_title= self.driver.title
+
+           if act_title=="Dashboard / nopCommerce administration":
+                self.driver.close()
+                assert True
+                
+
+           else:
+                self.driver.save_screenshot(".\\Screenshots\\"+"invalid_user.png")
+                self.driver.close()
+                assert False
+                
+            
+      
+     
+     
+     
